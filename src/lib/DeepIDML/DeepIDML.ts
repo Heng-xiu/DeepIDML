8f3a056124d66b2bca5a6f17f7a4648c0513c0b7
Heng-Shiou Sheu <r23456999@gmail.com>
2021-11-26 12:37:46 +0800

    fix(deepidml functionality): add new feature to parse raw IDML file directly, and extracting paragra

--- src/lib/DeepIDML/DeepIDML.ts
+++ src/lib/DeepIDML/DeepIDML.ts
@@ -3,6 +3,12 @@ import * as fxparser from 'fast-xml-parser';
 
 import { StyleTransformFactory } from '../styleTransfrom/StyleTransformFactory';
 
+type propertiesRecord = Record<string, any>;
+type paraGraphStyleRecord = Record<
+  string,
+  CSS.Properties & CSS.PropertiesHyphen
+>;
+
 export class DeepIDML implements IDeepIDML {
   private xml = '';
   private json: object = {};
@@ -26,8 +32,35 @@ export class DeepIDML implements IDeepIDML {
     };
     if (fxparser.validate(this.xml)) {
       this.json = fxparser.parse(this.xml, xmlParserOptions);
-    } else {
+    } 
+  }
+  parseXMLToJSON(xml: string) {
+    const xmlParserOptions = {
+      ignoreAttributes: false,
+      attributeNamePrefix: '@_',
+    };
+    if (fxparser.validate(xml)) {
+      return fxparser.parse(xml, xmlParserOptions);
     }
+  }
+  convertToCss(json: any) {
+    let tmpJson: any = {};
+    let tmpCssStyle = {};
+    const mappingKeys = this.factory.getKey();
+
+    Object.keys(json).forEach((key: string) => {
+      tmpCssStyle = {};
+      mappingKeys.forEach((prop: string) => {
+        const factory = this.factory.getStyleTransform(prop);
+        if (prop in json[key]) {
+          const result = factory.transformV2(json[key], prop);
+          tmpCssStyle = { ...tmpCssStyle, ...result };
+        }
+      });
+      tmpJson[key] = tmpCssStyle;
+    })
+    
+    return tmpJson;
   }
   convertStyleToCss() {
     if (
@@ -61,5 +94,44 @@ export class DeepIDML implements IDeepIDML {
     } else {
       return this.cssStyle;
     }
+  }
+  handleProperties(styleValue: any): object {
+    let properties: propertiesRecord = {};
+    Object.keys(styleValue['Properties']).forEach((prop) => {
+      let propName = '@_' + prop;
+      let propValue = {};
+      if (styleValue['Properties'][prop]['#text'] !== undefined) {
+        propValue = styleValue['Properties'][prop]['#text'];
+      } else {
+        propValue = styleValue['Properties'][prop];
+      }
+      properties = { ...properties, ...{ [propName]: propValue } };
+    });
+    return properties;
+  }
+  // parse default paragraph style groups
+  parseDefaultParagraphStyleGroups(xml: string) {
+    const jObj = this.parseXMLToJSON(xml);
+    const rootParaStyleGroup = jObj['idPkg:Styles']['RootParagraphStyleGroup']['ParagraphStyle'];
+    let defaultParaStyleMapList: paraGraphStyleRecord = {};
+
+    Object.keys(rootParaStyleGroup).forEach((key) => {
+      let paragraphStyleList = rootParaStyleGroup[key];
+      let styleName = paragraphStyleList['@_Name'].split('/')[1];
+      let styleValue = paragraphStyleList;
+      //Handle Properties property
+      let properties = this.handleProperties(styleValue);
+      if (styleValue.hasOwnProperty('Properties')) {
+        delete styleValue['Properties'];
+      }
+      let tmpStyle: paraGraphStyleRecord = {
+        [styleName]: { ...styleValue, ...properties },
+      };
+      defaultParaStyleMapList = {
+        ...defaultParaStyleMapList,
+        ...tmpStyle,
+      };
+    });
+    return this.convertToCss(defaultParaStyleMapList);
   }
 }
