aa9785f929d9f01d13815654e9cba07b1ddb45a5
Heng-Shiou Sheu <r23456999@gmail.com>
2021-11-27 15:24:02 +0800

    refactor(justificaiton): change justification recognize

--- src/lib/styleTransfrom/StyleTransformSet/JustificationStyleTransform.ts
+++ src/lib/styleTransfrom/StyleTransformSet/JustificationStyleTransform.ts
@@ -1,13 +1,15 @@
 import * as CSS from 'csstype';
 
 enum Justification_EnumValue {
-    LEFTALIGN = 'LeftAlign',
-    CENTERALIGN = 'CenterAlign',
-    RIGHTALIGN = 'RightAlign',
-    LEFTJUSTIFIED = 'LeftJustified',
-    RIGHTJUSTIFIED = 'RightJustified',
-    CENTERJUSTIFIED = 'CenterJustified',
-    FULLYJUSTIFIED = 'FullyJustified',
+    LEFTALIGN = 'LeftAlign',    //靠左對齊
+    CENTERALIGN = 'CenterAlign',//置中對齊
+    RIGHTALIGN = 'RightAlign',  //靠右對齊
+
+    LEFTJUSTIFIED = 'LeftJustified',    //(末行靠左對齊)
+    RIGHTJUSTIFIED = 'RightJustified',  //(末行靠右對齊)
+    CENTERJUSTIFIED = 'CenterJustified',//(末行置中對齊)
+    FULLYJUSTIFIED = 'FullyJustified',  //(強制對齊)
+
     TOBINDINGSIDE = 'ToBindingSide',
     AWAYFROMBINDINGSIDE = 'AwayFromBindingSide',
 }
@@ -30,8 +32,29 @@ export class JustificationStyleTransform implements StyleTransformStrategy {
         case Justification_EnumValue.RIGHTALIGN:
             style = { textAlign: 'right' };
             break;
+        case Justification_EnumValue.LEFTJUSTIFIED:
+            style = { 
+                textAlign: 'justify',
+                textAlignLast: 'left',
+            };
+            break;
+        case Justification_EnumValue.RIGHTJUSTIFIED:
+            style = { 
+                textAlign: 'justify',
+                textAlignLast: 'right',
+            };
+            break;
         case Justification_EnumValue.CENTERJUSTIFIED:
-            style = { textAlign: 'justify' };
+            style = { 
+                textAlign: 'justify',
+                textAlignLast: 'center',
+            };
+            break;
+        case Justification_EnumValue.FULLYJUSTIFIED:
+            style = { 
+                textAlign: 'justify',
+                textAlignLast: 'justify',
+            };
             break;
         default:
             style = { textAlign: 'left'};
